e89072d3fc54aeed9e5303c7005c13266a34f708
Alfaafox <155976811+Alfaafox@users.noreply.github.com>
2025-02-07 13:41:16 +0530

    vpc commit

--- main.tf
+++ main.tf
@@ -18,11 +18,30 @@ data "aws_vpc" "default" {
 default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1, "ap-south-1", "ap-south-1"]
+
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
 vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+subnet_id = module.blog_vpc.default_subnet[0]
 
  tags = {
     Name = "Learning Terraform"
@@ -32,6 +51,8 @@ vpc_security_group_ids = [module.blog_sg.security_group_id]
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
+
+  vpc_id = module.blog_vpc.vpc_id
   name    = "blog_new"
 
   vpc_id  = data.aws_vpc.default.id
