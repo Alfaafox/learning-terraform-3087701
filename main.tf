205b9ca81e46f9d68ef0b4bb242f223fcd0787e2
Alfaafox <155976811+Alfaafox@users.noreply.github.com>
2025-02-07 12:42:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,9 +32,9 @@ vpc_security_group_ids = [module.blog_sg.security_group_id]
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name = "blog_new"
+  name    = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id  = data.aws_vpc.default.id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -42,6 +42,20 @@ module "blog_sg" {
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group" "blog" {
+name = "blog"
+description = "Allow http and https in . Allow everyting out "
+
+vpc_id = data.aws_vpc.default.id
+
+ingress_rule        = ["http-80-tcp","https-443-tcp"]
+ingress_cidr_blocks = ["0.0.0.0/0"]
+
+egress_rule        = ["all-all"]
+egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 
 resource "aws_security_group_rule" "blog_http_in" {
 type          = "ingress"
