7ba435c8dc67707b4b41cc164697f7d9538191f6
Alfaafox <155976811+Alfaafox@users.noreply.github.com>
2025-02-07 12:26:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-vpc_security_group_ids = [module.blog_sg.security_id]
+vpc_security_group_ids = [module.blog_sg.security_group_id]
 
  tags = {
     Name = "Learning Terraform"
@@ -51,7 +51,7 @@ description = "Allow http and https in . Allow everyting out "
 
 vpc_id = data.aws_vpc.default.id
 
-ingress_rule        = ["http-80-tcp" , "https-443-tcp"]
+ingress_rule        = ["http-80-tcp","https-443-tcp"]
 ingress_cidr_blocks = ["0.0.0.0/0"]
 
 egress_rule        = ["all"]
