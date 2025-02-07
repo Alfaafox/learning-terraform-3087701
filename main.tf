3a8c9e9d9ebb8d01db8c0c1e6dfe025da420dfae
Alfaafox <155976811+Alfaafox@users.noreply.github.com>
2025-02-07 10:41:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,8 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+vpc_security_group_ids = [aws_security_blog_id]
+
  tags = {
     Name = "Learning Terraform"
   }
@@ -39,7 +41,7 @@ type          = "ingress"
 from_port     = 80
 to_port       = 80
 protocol      = "tcp"
-cidr_blocks   = [0.0.0.0/0]
+cidr_blocks   = ["0.0.0.0/0"]
 
 security_group_id = aws_security_group.blog.id
 }
@@ -49,7 +51,7 @@ type          = "ingress"
 from_port     = 443
 to_port       = 443
 protocol      = "tcp"
-cidr_blocks   = [0.0.0.0/0]
+cidr_blocks   = ["0.0.0.0/0"]
 
 security_group_id = aws_security_group.blog.id
 }
@@ -60,21 +62,9 @@ type          = "ingress"
 from_port     = 0
 to_port       = 0
 protocol      = "-1"
-cidr_blocks   = [0.0.0.0/0]
+cidr_blocks   = ["0.0.0.0/0"]
 
 security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog"
-module "blog_sg" { 
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-     name = "blog_new"
-  vpc_id  = data.aws_vpc.default.id
-ingress_rules = ["http-80-tcp","https-443-tcp"]
-ingress_cidr_blocks = ["0.0.0.0/0"]
 
-egress_rules = ["all-all"]
-egress_cidr_blocks = ["0.0.0.0/0"]
-
-}
